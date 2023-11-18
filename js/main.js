59a834a753810a8e60ce8381b01537f6493f23eb
LucasSoares98 <135660943+LucasSoares98@users.noreply.github.com>
2023-11-18 09:43:35 -0300

    commit initial

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    
+    $('#telefone').mask('(00) 00000-0000', {placeholder: '(__) _____-____'})
+
+    $('#cpf').mask('000.000.000-00', {placeholder: '___.___.___-__'})
+
+    $('#cep').mask('00000-000', {placeholder: '_____-___'})
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereço: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome'
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids()
+            if(camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos!`)
+            }
+        }
+    })
+})
